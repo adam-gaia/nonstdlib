80c47ebd2d97ad31162d564f39e10f360466c097
Adam Gaia <git@agaia.dev>
2024-10-11 09:40:34 -0600

    feat: start path module

---
+++ src/path.sh
@@ -0,0 +1,19 @@
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+  echo "$0 must be sourced, not executed."
+  exit 2
+fi
+
+# Include guard
+if [[ -n "${__STD_PATH_SOURCED+x}" ]]; then
+  __STD_PATH_SOURCED=1
+fi
+
+# TODO: then include guards might be broken for the same reason global vars had to be moved to the 'nonstdlib.sh' file
+
+# Make a path ($1) relative to another ($2)
+function path::relative() {
+  local path="$1"
+  local rel_to="$2"
+
+  realpath --relative-to="${rel_to}" "${path}"
+}
